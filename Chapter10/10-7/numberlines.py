9985d74a598ba32d82a217e029b89626e0f77d4d
nanyao <nanyao.hym@autonavi.com>
2019-09-21 22:43:09 +0800

    提交源码

---
+++ Chapter10/10-7/numberlines.py
@@ -0,0 +1,8 @@
+﻿# numberlines.py                                
+                                                   
+import fileinput                                   
+                                                   
+for line in fileinput.input(inplace=True):         
+    line = line.rstrip()                           
+    num = fileinput.lineno()                       
+    print('{:<50} # {:2d}'.format(line, num))
